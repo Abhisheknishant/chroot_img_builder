2f1d43a8136cd9a1f423ab1d1f6912756888aa98
Jonathan <jonathan.pastor@me.com>
2020-03-11 22:52:29 +0100

    add chroot script; add ssh file in boot partition

--- launch.sh
+++ launch.sh
@@ -13,6 +13,27 @@ KERNEL_IMG_URL="https://raw.githubusercontent.com/dhruvvyas90/qemu-rpi-kernel/ma
 # FUNCTIONS
 ############################
 
+function add_ssh_file_linux() {
+  echo "Add ssh file in Boot partition"
+  mkdir /mnt/mount_boot
+  START_SECTOR=$(fdisk -l result.img | grep W95 | awk '{print $2}')
+  START_POSITION=$(($START_SECTOR * 512))
+
+  mount -o offset=$START_POSITION result.img /mnt/mount_boot
+  touch /mnt/mount_boot/ssh
+  sync
+  umount /mnt/mount_boot
+
+  rm -rf /mnt/mount_boot
+}
+
+function add_ssh_file_macos() {
+    hdiutil mount 2020-02-13-raspbian-buster-lite.img -mountpoint /Volumes/boot
+    sleep 3
+    echo 'HelloWorld!' > /Volumes/boot/ssh
+    sleep 3
+    hdiutil unmount /Volumes/boot
+}
 
 ############################
 # CUSTOMIZE THE Boot partition
@@ -36,17 +57,28 @@ cp $RASPI_IMG $RASPI_WORK_IMG
 
 qemu-img resize $RASPI_WORK_IMG 5G
 
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	add_ssh_file_linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "ERROR: add ssh file is not yet implemtend on macos"
+	exit 1
+	add_ssh_file_macos
+else
+	echo "ERROR: add ssh file is not yet implemented on this system $OSTYPE"
+	exit 1
+fi
+
 ############################
 # Exit QEMU VM
 ############################
 qemu-system-arm \
+    -display none \
     -kernel kernel.img \
     -cpu arm1176 \
     -m 256 \
     -machine versatilepb \
     -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" \
     -drive "file=$RASPI_WORK_IMG,index=0,media=disk,format=raw" \
-    -serial stdio \
     -net nic -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::22280-:80 \
     -no-reboot \
 
